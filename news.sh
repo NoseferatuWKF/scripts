80392d8b1e93b348f15d1c9416a18673752a3d0f
NoseferatuWKF <wkf2584@gmail.com>
2024-02-19 23:34:19 +0800

    working on improving news.sh

--- news.sh
+++ news.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-bun ~/project/coding/javascript/swiper | fzf -m |
-tee --append ~/.news-histories |
-sed -E 's/.*(https)/\1/' |
-~/project/coding/go/distributor/distributor |
+bun ~/project/actually-useful/pnd/pump --url \
+https://news.ycombinator.com\?p= --selector .titleline --pages 2 --url \
+"https://lobste.rs/page/" --selector .link --pages 2 | fzf -m |
+tee --append ~/.news-histories | sed -E 's/.*(https)/\1/' |
+~/project/actually-useful/pnd/dump/dump |
 xargs -r vivaldi-stable --incognito --app 
